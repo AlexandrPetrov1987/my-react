207c055d339b26cd819f6246ac80f6068e06f060
alex <AlexandrPetrov1987>
2022-09-23 17:53:20 +0300

    add project

--- src/App.js
+++ src/App.js
@@ -3,9 +3,8 @@ import {Route, Routes} from "react-router-dom";
 function App() {
     return (
         <Routes>
-            <Route>
+            <Route/>
 
-            </Route>
         </Routes>
     );
 }
